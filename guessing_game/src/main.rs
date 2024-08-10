a01a5540563cfdb8e685da37eb87ef7ff691ec4b
adrian <adrianhgarza@gmail.com>
2024-08-10 13:55:15 -0700

    chapter 2 done

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,13 +1,37 @@
 use std::io::stdin;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the nummber");
 
     println!("Input your guess");
 
-    let mut guess = String::new();
+    let secret = rand::thread_rng().gen_range(1..=100);
 
-    stdin().read_line(&mut guess).expect("Didn't read line");
+    loop {
+        let mut guess = String::new();
+        stdin()
+            .read_line(&mut guess)
+            .expect("Didn't read line");
 
-    println!("You guessed: {guess}");
+        // SHADOWING: overwrite a previous variable name, replacing types
+        // match expression here
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret){
+            // a match expression consists of arms, each one is a pattern and then code that runs if it matches
+            Ordering::Less => println!("too low!"),
+            Ordering::Greater => println!("too high!"),
+            Ordering::Equal => {
+                println!("GOT EEM");
+                break;
+            }
+        }
+    }
 }
--- notes.md
+++ notes.md
@@ -1,9 +1,16 @@
-https://rust-book.cs.brown.edu/ch01-03-hello-cargo.html
+https://rust-book.cs.brown.edu/ch03-00-common-programming-concepts.html
 
 # Chapter 2
 - you need to explicitly specify mut, so a bit like JS/TS
 - `let` creates a variable but it's `const` by default
 - `&` is a reference, references are immutable by default, therefore just use `&mut` to signal something is a _mutable reference_
+- there's a lot of cool syntax for range expressions
+- println! is an exclamation mark because it's a macro for doing a lot of typesafe conversion stuff
+- **shadowing** is new, it lets you overwrite a previous variable with a new one (e.g. to change the type without making
+extra variable names)
+
+# Chapter 3
+
 
 ## Results
 - Enums, `Ok` and `Err` (rather than a return, err like Go)
